30b52ddc625a26041a16c72dee1dde12ad7373eb
Low Wei Hong <M140042@e.ntu.edu.sg>
2020-01-19 16:06:00 +0800

    minor changes release on setup for version 0.0.2

--- setup.py
+++ setup.py
@@ -1,28 +1,23 @@
-from setuptools import setup, find_packages
+import setuptools
 
-with open("README.md", "r") as f:
-    readme = f.read()
+# with open("README.md", "r") as f:
+#     readme = f.read()
 
-with open("requirements.txt", "r") as f:
-    reqs = [lib.strip() for lib in f if lib]
+# with open("requirements.txt", "r") as f:
+#     reqs = [lib.strip() for lib in f if lib]
 
-setup(
+setuptools.setup(
     name="us_news",
-    version="0.0.1",
+    version="0.0.2",
     description="A web crawler to crawl Best Global University Ranking on usnews website",
     long_description=readme,
     author='Low Wei Hong',
     author_email='M140042@e.ntu.edu.sg',
     url="https://github.com/M140042/us_news",
-    packages=['us_news'],
+    packages=setuptools.find_packages(),
     keywords="us_news",
     classifiers=[
         "Programming Language :: Python :: 3",
     ],
-    entry_points={
-        'console_scripts': [
-            'dinner=dinner:main',
-        ]
-    },
-    install_requires=reqs
+    install_requires=['pandas', 'httpx']
 )
